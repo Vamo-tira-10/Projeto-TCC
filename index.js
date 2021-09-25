b9ea44703f6148bdeba49c14a1998237a1ab8283
André Honorato <gamera103@gmail.com>
2021-09-25 14:38:19 -0300

    Integrando tela de cadastro a base do projeto

--- index.js
+++ index.js
@@ -14,30 +14,42 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+//Rota de cadastro de usuários
+app.get('/users/new', (req, res) => {
+    res.render('users/new')
+})
+
+//Rota de login de usuários
 app.get('/users/login', (req, res) => {
     res.render('users/login')
 })
 
+//Rota de painel do usuário
 app.get('/users/panel', (req, res) => {
     res.render('users/panel')
 })
 
+//Rota de Flash Cards do usuário
 app.get('/users/panel/flashcards/new', (req, res) => {
     res.render('users/flashcards/new')
 })
 
+//Rota de Caderno Virtual do usuário
 app.get('/users/panel/notebook/new', (req, res) => {
     res.render('users/notebook/new')
 })
 
+//Rota de Agenda do usuário
 app.get('/users/panel/schedule/new', (req, res) => {
     res.render('users/schedule/new')
 })
 
+//Rota de painel do admin
 app.get('/admin/panel', (req, res) => {
     res.render('users/admin/panel')
 })
 
+//Rota de erro 404
 app.get('/*', (req, res) => {
     res.render('404')
 })
